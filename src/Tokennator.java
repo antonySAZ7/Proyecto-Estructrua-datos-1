3f4400f018db35fd79f608263391d9375334919f
DeividArriaza <lop24730@uvg.edu.gt>
2025-03-17 21:31:14 -0600

    Se ha ajustado el código, agregado una función para comprobar si hay caracteres en la expresión y se está revisando la causa de un posible error en la lectura de las expresiones de lisp

--- src/Tokennator.java
+++ src/Tokennator.java
@@ -5,50 +5,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tokennator {
-
-
     public List<String> tokennate(String entranteCodigo){
         List<String> tokens = new ArrayList<>();
         StringBuilder token =  new StringBuilder();
         int contadorParentesis = 0;
+
         for (char c : entranteCodigo.toCharArray())  {
-           if(Character.isWhitespace(c)){
-            if(token.length() > 0){
-                tokens.add(token.toString());
-                token.setLength(0);
+            //Agregar caracter de espacio en blanco
+            if(Character.isWhitespace(c)){
+                if(hasChar(token)){
+                    tokens.add(token.toString());
+                    token.setLength(0);
+                }
             }
-           }else if(c =='('){
-            contadorParentesis++;
-            if(token.length()>0){
-                tokens.add(token.toString());
-                token.setLength(0);
+            //Agregar inicio de parentesis
+            else if(c =='('){
+                contadorParentesis++;
+                if(hasChar(token)){
+                    tokens.add(token.toString());
+                    token.setLength(0);
+                }
+                tokens.add("(");
             }
-            tokens.add("(");
-           }else if (c == ')'){
-            contadorParentesis--;
-            if(contadorParentesis < 0){
-                throw new IllegalArgumentException("Error en la sintaxis, revisa la cantidad de parentesis");
+            //Agregar cierre de paréntesis
+            else if (c == ')'){
+                contadorParentesis--;
+                if(contadorParentesis < 0){
+                    throw new IllegalArgumentException("Error en la sintaxis, revisa la cantidad de parentesis");
+                }
+                
+                if(hasChar(token)){
+                    tokens.add(token.toString());
+                    token.setLength(0);
+                }
+                token.append(")");
+            } 
+
+            else {
+                token.append(c);
             }
-            if(token.length()>0){
-                tokens.add(token.toString());
-                token.setLength(0);
-            }
-            token.append(")");
-           } else {
-            token.append(c);
-           }
         }
-        if(token.length() > 0 ){
+        if(hasChar(token)){
             tokens.add(token.toString());
         }        
 
         if(contadorParentesis != 0 ){
             throw new IllegalArgumentException("Error en la sistaxis, posiblemente falten o sobren parentesis");
         }
+
         return tokens;
 
 
     }
-
     
+    public boolean hasChar(StringBuilder token){
+        return token.length()>0;
+    }
 }
